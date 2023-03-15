09d30e3954b7e5d2b6e56626f236e4c3a562c452
sparkarius <114961794+sparkarius@users.noreply.github.com>
2023-03-15 00:38:12 -0400

    updating assets

---
+++ 1tickt/assets/js/videoPlayerActions.js
@@ -0,0 +1,58 @@
+function likeVideo(button, videoId) {
+    // ajax call submission
+    $.post("ajax/likeVideo.php", {videoId: videoId})
+    .done(function(data) {
+        // using jQuery for button 
+        var likeButton = $(button);
+        var dislikeButton = $(button).siblings(".dislikeButton");
+
+        likeButton.addClass("active");
+        dislikeButton.removeClass("active");
+
+        var result = JSON.parse(data);
+        updateLikesValue(likeButton.find(".text"), result.likes);
+        updateLikesValue(dislikeButton.find(".text"), result.dislikes);
+
+        if(result.likes < 0) {
+            likeButton.removeClass("active");
+            likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up.png");
+        }
+        else {
+            likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up-active.png");
+        }
+
+        dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down.png");
+    }); 
+}
+
+function dislikeVideo(button, videoId) {
+    // ajax call submission
+    $.post("ajax/dislikeVideo.php", {videoId: videoId})
+    .done(function(data) {
+        // using jQuery for button 
+        var dislikeButton = $(button);
+        var likeButton = $(button).siblings(".likeButton");
+
+        dislikeButton.addClass("active");
+        likeButton.removeClass("active");
+
+        var result = JSON.parse(data);
+        updateLikesValue(likeButton.find(".text"), result.likes);
+        updateLikesValue(dislikeButton.find(".text"), result.dislikes);
+
+        if(result.dislikes < 0) {
+            dislikeButton.removeClass("active");
+            dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down.png");
+        }
+        else {
+            dislikeButton.find("img:first").attr("src", "assets/images/icons/thumb-down-active.png");
+        }
+
+        likeButton.find("img:first").attr("src", "assets/images/icons/thumb-up.png");
+    }); 
+}
+
+function updateLikesValue(element, num) {
+    var likesCountVal = element.text() || 0; // default to 0 if element is not found 
+    element.text(parseInt(likesCountVal) + parseInt(num)); // Convert string to int to update value  
+}
